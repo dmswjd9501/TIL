bf9f610c52b987ebf7bb14bda3cdb8b853212df7
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-07-24 17:49:36 +0900

    190724 | python - day6 | OOP_basic

--- python/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-key = '4541b77c39ed819f55b6d3b2da045cb6'
-targetDt = '20190713' #yyyymmdd
-api_url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={key}&targetDt={targetDt}'
-print(api_url)
-response = requests.get(api_url).json()
-print(response)
